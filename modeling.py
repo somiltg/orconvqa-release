Fix: model softmax

modeling.py
@@ -960,8 +960,9 @@ class AlbertWithHAMForRetrieverOnlyPositivePassage(AlbertForRetrieverOnlyPositiv
             print("query_sequence_tokens shape {}".format(query_sequence_tokens.shape))
             print('query sequence reps shape {}'.format(query_sequence_reps.shape))
             alphas = torch.nn.functional.softmax(cls_weights, dim=0)  # calculate probabilities for history attention scores.
+            alphas = torch.unsqueeze(alphas, dim=-1)
             print("alpha  values {}".format(alphas))
-            dense_representation = torch.sum(query_sequence_reps * alphas, dim=1)
+            dense_representation = torch.sum(query_sequence_reps * alphas, dim=0)
             print("dense representation shape {}".format(dense_representation.shape))
             output.append(dense_representation)
         output = torch.cat(output, dim=0)